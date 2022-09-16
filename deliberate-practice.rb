7f779911fc6f81d8e0898b26a7ad555d93420b47
DanielIbrag <danny.ibragimov1@gmail.com>
2022-09-16 16:33:15 -0400

    problem 4 ruby conditionals solved

--- deliberate-practice.rb
+++ deliberate-practice.rb
@@ -9,11 +9,27 @@
 
 #Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 
-var = 11
-if var < 10
-  p -1
-elsif var > 10
+# var = 11
+# if var < 10
+#   p -1
+# elsif var > 10
+#   p 1
+# else
+#   p 0
+# end
+
+#Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+# digit1 = 4
+# digit2 = 11
+# if digit1 && digit2 < 10
+#   p 1
+# else
+#   p 0
+# end
+# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+var = 900
+if var > 9000
   p 1
 else
-  p 0
+  p -1
 end
