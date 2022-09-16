9941ef9d0c5cd1415c218bf916893d48e40237fe
DanielIbrag <danny.ibragimov1@gmail.com>
2022-09-16 13:47:47 -0400

    solving a Skills problem in ruby

--- deliberate-practice.rb
+++ deliberate-practice.rb
@@ -1,9 +1,19 @@
-def if_10(number)
-  if number == 10
-    p 0
-  else
-    p -1
-  end
-end
+# Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+# def if_10(number)
+#   if number == 10
+#     p 0
+#   else
+#     p -1
+#   end
+# end
 
-if_10(0)
+#Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+var = 11
+if var < 10
+  p -1
+elsif var > 10
+  p 1
+else
+  p 0
+end
