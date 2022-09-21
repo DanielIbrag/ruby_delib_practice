45ecec2ce430ff627d80c0a33a1bf7101bff09ba
DanielIbrag <danny.ibragimov1@gmail.com>
2022-09-21 13:38:18 -0400

    solved question 8 in ruby

--- deliberate-practice.rb
+++ deliberate-practice.rb
@@ -66,6 +66,14 @@
 # end
 
 # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+var = 10
+if var == 100
+  print 100
+elsif var == 99
+  print 99
+else
+  print 0
+end
 
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
 
