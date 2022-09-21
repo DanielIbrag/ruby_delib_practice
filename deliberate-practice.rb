aa648ce7f8e4443b1dc4fe7ca044e283986a27cc
DanielIbrag <danny.ibragimov1@gmail.com>
2022-09-21 13:26:04 -0400

    added all questions and answered question 7

--- deliberate-practice.rb
+++ deliberate-practice.rb
@@ -45,3 +45,28 @@
 # else
 #   print -1
 # end
+
+# 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+# var1 = 11
+# var2 = 7
+
+# if var1 > 10 || var2 > 10
+#   p 100
+# else
+#   p -100
+# end
+
+# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+# var = 1
+# if var < 0
+#   print 1776
+# else
+#   print 1979
+# end
+
+# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+# 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
