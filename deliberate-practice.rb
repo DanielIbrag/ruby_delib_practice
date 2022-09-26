a2320c715e5a2aaf4790e899bd35eaae81e2fc8f
DanielIbrag <danny.ibragimov1@gmail.com>
2022-09-26 14:07:23 -0400

    problems one and two for interpolation completed

--- deliberate-practice.rb
+++ deliberate-practice.rb
@@ -177,3 +177,29 @@
 #   return new_array
 # end
 # p moveover([2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2])
+
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+puts "what is your First name?"
+first_name = gets.chomp
+puts "what is your last name?"
+last_name = gets.chomp
+
+puts "your first name is " + first_name + " and your last name is " + last_name
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+puts "your first name is #{first_name} and your last name is #{last_name}"
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+# 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+# 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+# 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
