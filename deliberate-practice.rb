ab2c76bc911f1ce6566c821c505e0cc5f523a3ee
DanielIbrag <danny.ibragimov1@gmail.com>
2022-09-27 14:01:52 -0400

    working on problem 4 ruby

--- deliberate-practice.rb
+++ deliberate-practice.rb
@@ -192,15 +192,21 @@
 # puts "what is your last name?"
 # puts "your first name is #{first_name} and your last name is #{last_name}"
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
-puts "please input a word!"
-word = gets.chomp
-if word == "marco"
-  print "Polo!"
-else
-  print word
-end
+# puts "please input a word!"
+# word = gets.chomp
+# if word == "marco"
+#   print "Polo!"
+# else
+#   print word
+# end
 
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+puts "Please input a color! "
+color1 = gets.chomp
+puts "Please input a second color! "
+color2 = gets.chomp
+puts "Please input a third color! "
+color3 = gets.chomp
 
 # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
 
